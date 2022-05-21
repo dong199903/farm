789888680b3e77dfc11195217e905b6acc7a7969
董泽平 <5296738+tseping_dong@user.noreply.gitee.com>
2022-05-21 15:16:28 +0800

    解决冲突

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,53 +1,185 @@
-/*
- * @description: 
- * @author: 董泽平
- */
 import swipers from "./../../services/swiper/index"
 import sorts from "./../../services/sort/index"
-import command from "./../../services/home/command"
 Page({
   async onLoad() {
-    //获取轮播图数据
     let swiper = await swipers()
-    //获取所有分类数据
-    let sort = await sorts()
-    //获取热门推荐商品
-    let commands = await command()
-
-    //截取前5个分类展示，第六个分类是热门推荐
-    let sortUp = sort.result.data.slice(0,5)
-    console.log("swiper",swiper)
-    console.log("sort",sort)
-    console.log("command",commands.result.data)
     this.setData({
       items: swiper.result.data,
-      sort:sortUp,
-      command:commands.result.data
+      sort:sortUp
     })
   },
   data: {
-    current: 0,
-    items: []
-  },
-  toSort(e){
-    wx.reLaunch({
-      url: './../goods/list/index?id='+ e.currentTarget.dataset.id +'&title='+e.currentTarget.dataset.name
-    })
-  },
-  onChange(e) {
-    const {
-      detail: {
-        current,
-        source
+    current: 1,
+    background: [
+      {
+        image: `../../style/images/轮播图.png`,
+      },{
+        image: `../../style/images/轮播图.png`,
+      },{
+        image: `../../style/images/轮播图.png`,
+      },{
+        image: `../../style/images/轮播图.png`,
+      },{
+        image: `../../style/images/轮播图.png`,
       },
-    } = e;
-    console.log(current, source)
+    ],
+    category: [
+      {
+        id: '',
+        text: '农禽产品',
+        image: `../../style/images/鸡蛋箱.png`,
+      }, {
+        id: '',
+        text: '农禽产品',
+        image: `../../style/images/鸡蛋箱.png`,
+      }, {
+        id: '',
+        text: '农禽产品',
+        image: `../../style/images/鸡蛋箱.png`,
+      }, {
+        id: '',
+        text: '农禽产品',
+        image: `../../style/images/鸡蛋箱.png`,
+      }
+    ],
+    goods: [
+      {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }, {
+        "_id":"dksekr1fajfada",
+        "sid":"1",
+        "price1":99,
+        "price2":88,
+        "img":`../../style/images/蔬菜水果.png`,
+        "desc":"物美价廉",
+        "title":"水果"
+      }
+    ]
   },
-
-  sub(){
-    wx.navigateTo({
-      url: './../goods/search/index',
-    });
-      
-  }
 })
