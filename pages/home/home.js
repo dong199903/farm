8dc4309e714c0b6b1ca5b835f49c8ea434cab341
RECHAHotTee <Pipika007@163.com>
2022-05-21 16:07:42 +0800

    recha

--- pages/home/home.js
+++ pages/home/home.js
@@ -27,30 +27,30 @@ Page({
       {
         id: '',
         text: '农禽产品',
-        image: `../../style/images/鸡蛋箱.png`,
+        image: `../../style/images/蔬菜水果.png`,
       }, {
         id: '',
         text: '农禽产品',
-        image: `../../style/images/鸡蛋箱.png`,
+        image: `../../style/images/蔬菜水果.png`,
       }, {
         id: '',
         text: '农禽产品',
-        image: `../../style/images/鸡蛋箱.png`,
+        image: `../../style/images/蔬菜水果.png`,
       }, {
         id: '',
         text: '农禽产品',
-        image: `../../style/images/鸡蛋箱.png`,
+        image: `../../style/images/蔬菜水果.png`,
       }
     ],
     goods: [
       {
         "_id":"dksekr1fajfada",
         "sid":"1",
-        "price1":99,
-        "price2":88,
+        "price1":9999,
+        "price2":88888,
         "img":`../../style/images/蔬菜水果.png`,
-        "desc":"物美价廉",
-        "title":"水果"
+        "desc":"物美价廉物美价廉物美价廉物美价廉物美价廉物美价廉物美价廉物美价廉物美价廉",
+        "title":"水果水果水果水果水果水果水果水果水果水果水果水果水果水果"
       }, {
         "_id":"dksekr1fajfada",
         "sid":"1",
