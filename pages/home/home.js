62bd90170d058c309a482e82e5e35011a4696921
董泽平 <5296738+tseping_dong@user.noreply.gitee.com>
2022-05-24 20:29:04 +0800

    冲突解决

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 董泽平 5296738+tseping_dong@user.noreply.gitee.com
+ * @Date: 2022-05-21 11:23:38
+ * @LastEditors: 董泽平 5296738+tseping_dong@user.noreply.gitee.com
+ * @LastEditTime: 2022-05-24 20:25:19
+ * @FilePath: \07-reactc:\Users\19394\Desktop\farm\pages\home\home.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import swipers from "./../../services/swiper/index"
 import sorts from "./../../services/sort/index"
 import command from "./../../services/command/index"
@@ -9,21 +17,15 @@ Page({
     let sort = await sorts()
     //热门推荐的商品
     let commands = await command()
-
-    let sortUp = sort.result.data.slice(0,5)
     this.setData({
       swiper: swiper.result.data,
-      sort:sortUp.result.data,
+      sort:sort.result.data,
       command:commands.result.data,
       loading:false
     })
   },
-  data: {
-    current: 1,
-    swiper: [],
-    category: [],
-    recommend_goods: [],
-    loading: true
+  data:{
+    loading:true
   },
   goSearch: function () {
     wx.navigateTo({
@@ -32,9 +34,8 @@ Page({
   },
   goCategory: function (e) {
     let id = e.currentTarget.dataset.cateid
-    let title = e.currentTarget.dataset.catetitle
     wx.navigateTo({
-      url: '/pages/category/category?id=' + id + '&title=' + title
+      url: '/pages/category/category?_id=' + id
     })
   },
   categoryTap: function (e) {
