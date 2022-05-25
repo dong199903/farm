d245c102cbb0ebb550393b0859a7305012f49909
董泽平 <5296738+tseping_dong@user.noreply.gitee.com>
2022-05-25 11:55:49 +0800

    修改我的页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,11 +1,3 @@
-/*
- * @Author: 董泽平 5296738+tseping_dong@user.noreply.gitee.com
- * @Date: 2022-05-21 11:23:38
- * @LastEditors: 董泽平 5296738+tseping_dong@user.noreply.gitee.com
- * @LastEditTime: 2022-05-24 20:25:19
- * @FilePath: \07-reactc:\Users\19394\Desktop\farm\pages\home\home.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import swipers from "./../../services/swiper/index"
 import sorts from "./../../services/sort/index"
 import command from "./../../services/command/index"
